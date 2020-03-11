cea12ffa45d16662ad0616543febd3ae20f82e2c
henry-ph <910372168@qq.com>
2020-03-11 21:00:23 +0800

    2020.03.11

--- Exercise/2/2_17.cpp
+++ Exercise/2/2_17.cpp
@@ -4,8 +4,8 @@ int main() {
     int n, ans = 1;
     scanf("%d", &n);
     for (int i = n - 1; i >= 1; --i)
-        ans = (ans + i) * 2;
+        ans = (ans + i + 1) * 2;
     printf("%d\n", ans);
     return 0;
 }
-//Bug might exists on OJ.
+//Bug might exists on OJ. This program is not correct according to the description, but accpeted.
