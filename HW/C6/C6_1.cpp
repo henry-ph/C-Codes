ded2d96604312378c3b84288d51797e3fc885795
henry-ph <910372168@qq.com>
2020-04-20 21:59:21 +0800

    2020.04.20

--- HW/C6/C6_1.cpp
+++ HW/C6/C6_1.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 #include<cmath>
+#include<cstring>
 
 using namespace std;
 
-const int N = 5e3 + 5;
+const int N = 1e5 + 5, inf = 0x3f3f3f3f;
 int a[N], dp[N];
 
 int main() {
-    int n, ans = 0;
+    int n, ans = -inf;
+    memset(dp, -inf, sizeof(dp));
     cin >> n;
     for (int i = 1; i <= n; ++i)
         cin >> a[i];
