591364e297882bb31a8553cb853e75caf6c9fcb8
henry-ph <910372168@qq.com>
2020-05-06 09:59:43 +0800

    2020.05.06

--- in_class.cpp
+++ in_class.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
 int func(int x, int &y) {
@@ -9,7 +10,24 @@ int func(int x, int &y) {
     return x + y;
 }
 int main() {
-    int x = 0, y = 1;
-    cout << func(x, y) << ' ' << x << ' ' << y << endl;
+    vector<int> a;
+    vector<int> c;
+    vector<int *> b;
+    vector<int>::iterator it;
+    vector<int *>::iterator _it;
+    for (int i = 0; i < 10; ++i) {
+        a.push_back(i);
+    }
+    for (it = a.begin(); it != a.end(); ++it)
+    {
+        c.push_back(*it);
+        b.push_back(&(c.back()));
+        //b.push_back(&(*it));
+    }
+    int sum = 0;
+    for (_it = b.begin(); _it != b.end(); ++_it) {
+        sum += **_it;
+    }
+    cout << sum << endl;
     return 0;
 }
---
+++ tmp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+    char* a;
+    getchar();
+    a
+    while (a != '\n') {
+        getchar();
+    }
+    cout << a << endl;
+    return 0;
+}
