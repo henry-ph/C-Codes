b474078a0fa2e3dec61e0200c29a814d7a68ea80
henry-ph <910372168@qq.com>
2020-05-10 08:46:54 +0800

    2020.05.10

--- in_class.cpp
+++ in_class.cpp
@@ -1,33 +1,11 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <cstring>
 using namespace std;
-
-int func(int x, int &y) {
-    int tmp;
-    tmp = x;
-    x = y;
-    y = tmp;
-    return x + y;
-}
-int main() {
-    vector<int> a;
-    vector<int> c;
-    vector<int *> b;
-    vector<int>::iterator it;
-    vector<int *>::iterator _it;
-    for (int i = 0; i < 10; ++i) {
-        a.push_back(i);
-    }
-    for (it = a.begin(); it != a.end(); ++it)
-    {
-        c.push_back(*it);
-        b.push_back(&(c.back()));
-        //b.push_back(&(*it));
-    }
-    int sum = 0;
-    for (_it = b.begin(); _it != b.end(); ++_it) {
-        sum += **_it;
-    }
-    cout << sum << endl;
+class A{
+public:
+    virtual void fun();
+};
+int main(int argc, const char * argv[]) {
+    cout << sizeof(A) << endl;
     return 0;
 }
